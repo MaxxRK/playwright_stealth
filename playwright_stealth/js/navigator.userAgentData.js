1ce348ca2306b609f372d44d06da6a247cd1fc72
Michael Bianco <iloveitaly@gmail.com>
2024-05-31 22:04:17 +0200

    more accurate user agent data mock

--- playwright_stealth/js/navigator.userAgentData.js
+++ playwright_stealth/js/navigator.userAgentData.js
@@ -7,7 +7,8 @@ const uaVersion = ua.match(/Chrome\/([\d|.]+)/)[1]
 
 // Source in C++: https://source.chromium.org/chromium/chromium/src/+/master:components/embedder_support/user_agent_utils.cc;l=55-100
 const _getBrands = () => {
-  const seed = uaVersion.split('.')[0] // the major version number of Chrome
+  // const seed = uaVersion.split('.')[0] // the major version number of Chrome
+  const seed = 124
 
   const order = [
     [0, 1, 2],
@@ -46,8 +47,65 @@ const metadata = {
   brands: _getBrands()
 }
 
-console.log("hi mike")
+const highEntropyValues = {
+    "architecture": "arm",
+    "bitness": "64",
+    "brands": [
+        {
+            "brand": "Chromium",
+            "version": "124"
+        },
+        {
+            "brand": "Google Chrome",
+            "version": "124"
+        },
+        {
+            "brand": "Not-A.Brand",
+            "version": "99"
+        }
+    ],
+    "fullVersionList": [
+        {
+            "brand": "Chromium",
+            "version": "124.0.6367.208"
+        },
+        {
+            "brand": "Google Chrome",
+            "version": "124.0.6367.208"
+        },
+        {
+            "brand": "Not-A.Brand",
+            "version": "99.0.0.0"
+        }
+    ],
+    "mobile": false,
+    "model": "",
+    "platform": "macOS",
+    "platformVersion": "14.4.1",
+    "uaFullVersion": "124.0.6367.208"
+}
+
+const userAgentMetadata = {
+  ...metadata,
+}
+
+Object.defineProperty(userAgentMetadata, 'getHighEntropyValues', {
+  value: function(hints) {
+    return new Promise((resolve, reject) => {
+      let result = {};
+
+      hints.forEach(hint => {
+        if (highEntropyValues[hint]) {
+          result[hint] = highEntropyValues[hint];
+        }
+      });
+
+      resolve(result);
+    });
+  },
+  enumerable: false
+});
 
 Object.defineProperty(Object.getPrototypeOf(navigator), 'userAgentData', {
-    get: () => metadata
+    get: () => userAgentMetadata
 })
