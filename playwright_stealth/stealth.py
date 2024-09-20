a9046221978e256a2ca501f29b69fc9883d926a6
maxxrk <drgullett89@outlook.com>
2024-09-19 21:34:46 -0500

    try some more

--- playwright_stealth/stealth.py
+++ playwright_stealth/stealth.py
@@ -105,7 +105,7 @@ class StealthConfig:
         )
 
         # defined options constant
-        yield f"window.opts = {opts}; console.log('opts defined:', window.opts);"
+        yield f"const opts = {opts}; console.log('opts defined:', opts);"
         # init utils and generate_magic_arrays helper
         yield SCRIPTS["utils"]
         yield SCRIPTS["generate_magic_arrays"]
@@ -131,7 +131,7 @@ class StealthConfig:
         ]
 
         for key in script_keys:
-            yield SCRIPTS[key]
+            yield f"{SCRIPTS[key]}; console.log('Script {key} executed with opts:', opts);"
 
 
 def stealth_sync(page: SyncPage, config: StealthConfig = None):
